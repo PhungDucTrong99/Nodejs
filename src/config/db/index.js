a09b800ff06ecaddb468e476f32887ceab8326c0
trong99 <phungductrong1999@gmail.com>
2023-08-23 00:06:04 +0700

    first commit

---
+++ src/config/db/index.js
@@ -0,0 +1,13 @@
+const mongoose = require("mongoose");
+
+async function connect() {
+    try {
+        await mongoose.connect("mongodb://localhost:27017/CRUD", {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+          });
+    } catch (error) {
+        console.log("connect fails");
+    }
+}
+module.exports = { connect };
