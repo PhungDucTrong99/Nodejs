f6fb66c587a8a6fb0b2ba26701c0ad36b9604d62
trong99 <phungductrong1999@gmail.com>
2023-08-23 21:41:18 +0700

    find data for mongodb

--- src/index.js
+++ src/index.js
@@ -1,10 +1,27 @@
 const express = require('express');
+const { default: mongoose } = require('mongoose');
 const app = express();
-const port = 3000;
+const port = 3005;
 
 const db = require('./config/db/index'); // Đường dẫn tới tệp connect.js
 
-db.connect(); // Gọi hàm connect từ tệp connect.js
+// db.connect(); // Gọi hàm connect từ tệp connect.js
+mongoose.connect("mongodb://localhost:27017/Test1");
+
+const UserSchema = new mongoose.Schema({
+  name: String,
+  age: String
+})
+
+const UserModel = mongoose.model("users", UserSchema)
+
+app.get("/getUsers", (req, res) => {
+  UserModel.find({}).then(function(user) {
+    res.json(user)
+  }).catch(function(err) {
+    console.log(err)
+  })
+})
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
